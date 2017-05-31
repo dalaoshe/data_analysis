64f3081ef3f64fd08e1ded5ff6fdec96d6437852
dalaoshe <dalaoshe@dalaoshe>
2017-05-31 15:33:47 +0800

    complete mifa/jacobi/sor

--- jacobi.py
+++ jacobi.py
@@ -3,95 +3,96 @@
 import numpy as np
 from Hilbert_Cholesky import create_H
 
-raws = 3
-cols = 3
-
-data_file = "data.txt"
-A = list()
-B = list()
-x = list()
-e = 0
-
-file = open(data_file,"r")
+# raws = 3
+# cols = 3
 
 
-for i in range(raws):
-    ai = [float(x) for x in file.readline().strip().split()]
-    A.append(ai)
-
-B = [float(x) for x in file.readline().strip().split()]
-x = [float(x) for x in file.readline().strip().split()]
-e = float(file.readline().strip())
-
-A = np.array(A)
-B = np.array(B)
-x = np.array(x)
-print A, B, A.dtype, B.dtype
-#
-#
-# for i in range(raws):
-#     for j in range(cols):
-#         print "%d " % A[i][j],
-#
-#     print ""
-#
-# for i in range(raws):
-#     print "%d " % B[i],
-#
-# print ""
 
 
 def get_e(x_k, x_k_1):
-    print x_k, " \n", x_k_1
-    x = np.max(np.fabs(x_k - x_k_1))
+    #print "x_k ",x_k, " \nx_k_1", x_k_1
+    x = np.max(np.fabs(np.array(x_k) - np.array(x_k_1)))
     return x
 
 
 
 
-def jacobi(A, B, x, e):
+def jacobi(A, B, x, e, dimession):
     k_e = 555555
     k = 0
+    e_list = list()
     while k_e > e:
         y = list()
-        for i in range(raws):
+        for i in range(dimession):
             y.append(x[i])
-        for i in range(raws):
+        for i in range(dimession):
             first = 0
             second = 0
             for j in range(0, i):
                 first = first + A[i][j] * y[j]
-                print "i %d " % i, " j %d" % j, " Aij %f" % A[i][j], " first %f" % first
-            for j in range(i+1, raws):
-                second = second + A[i][j] * y[j]
 
+            for j in range(i+1, dimession):
+                second = second + A[i][j] * y[j]
+            #print "i %d " % i, " j %d" % j, " Aij %f" % A[i][j], " first %f" % first, " second %f" %second, " B[i] %f" %B[i]
             x[i] = (B[i] - first - second) / A[i][i]
         k_e = get_e(x, y)
         k = k + 1
+        print "iteration_k:: %d" % k, " e: %f" % k_e
+        print " x: ", x
+        e_list.append(k_e)
         print "k: %d" % k, " k_e: %f" % k_e, " x: ", x
+    return e_list, x
 
-def SOR(A, B, w, x, e):
+
+def SOR(A, B, w, x, e, dimession):
     k_e = 555555
     k = 0
+    e_list = list()
     while k_e > e:
         y = list()
-        for i in range(raws):
+        for i in range(dimession):
             y.append(x[i])
-        for i in range(raws):
+        for i in range(dimession):
             first = 0
             second = 0
             for j in range(0, i):
                 first = first + A[i][j] * x[j]
-                print "i %d " % i, " j %d" % j, " Aij %f" % A[i][j], " first %f" % first
-            for j in range(i+1, raws):
+                #print "i %d " % i, " j %d" % j, " Aij %f" % A[i][j], " first %f" % first
+            for j in range(i+1, dimession):
                 second = second + A[i][j] * x[j]
 
             x[i] = (1.0 - w) * x[i] + w * (B[i] - first - second) / A[i][i]
         k_e = get_e(x, y)
         k = k + 1
-        print "k: %d" % k, " k_e: %f" % k_e, " x: ", x
+        e_list.append(k_e)
+        #print "k: %d" % k, " k_e: %f" % k_e, " x: ", x
+    return e_list, x, k
 
-#SOR(A, B, 0.9, x, e)
-jacobi(A, B, x, e)
-print "result:"
-print np.dot(A,x)
+def test_SOR_w(step=0.01):
+    A = create_H(10)
+    b = [float(1.0/x) for x in range(1,11)]
+    e = 1e-4
+    x_0 = [float(0.0) for x in range(10)]
+    x_a = [float(0.0) for x in range(10)]
+    x_a[0] = 1.0
+    print "A ",A
+    print "b ",b
+    print "x ", x_0
+    w = step
+    w_es = list()
+    w_ks = list()
+    w_s = list()
+    while w < 2.0:
+        x_0 = [float(0.0) for x in range(10)]
+        e_list, x_p, k = SOR(A, b, w, x_0, e, 10)
+        error = (get_e(x_a, x_p))/1.0
+        print "iteration:%d, error:%0.10f " %(k, error)
+        print "p_result:",x_p
+        w_s.append(w)
+        w_ks.append(k)
+        w_es.append(error)
+
+        w += step
+
+if __name__ == '__main__':
+    test_SOR_w(0.1)
--- mifa_u.py
+++ mifa_u.py
@@ -1,55 +1,77 @@
 import numpy as np
 from numpy import *
 
-def can_stop(u1 , u2):
-    return (u1-u2) < 0.0001 and u1-u2 > -0.0001
+def can_stop(u1 , u2, e):
+    return abs(u1-u2) < e
 
 
-def get_u(A, v):
-    u = v
+def get_u(A, v, e):
+    u = v.copy()
     l_1 = 0
-    l_2 = 10.0
-    while not can_stop(l_1,l_2):
+    l_2 = 100000000000000.0
+    k = 0
+    while not can_stop(l_1, l_2, e):
         l_1 = l_2
         v = np.dot(A, u)
-        print"v: ", v
-        l_2 = v.max()
+        l_2 = np.abs(v).max()
+        #print l_2
         u = v / l_2
-        print "l_2: ",l_2
-        print "u: ",u
-    return u, l_2
+        k += 1
+    return u, l_2, k
 
-def get_u_r(A, v):
+
+def get_u_r(A, v, e):
     u = v
     l_1 = 0
     l_2 = 10.0
     A = np.linalg.inv(A)
     print A
 
-    while not can_stop(l_1,l_2):
+    while not can_stop(l_1, l_2, e):
         l_1 = l_2
         v = np.dot(A, u)
         print"v: ", v
-        l_2 = 1.0 / v.max()
+        l_2 = 1.0 / np.abs(v).max()
         u = v * l_2
         print "l_2: ",l_2
         print "u: ",u
     return u, l_2
 
 
-A = np.array([
-    [7,3,-2],
-    [3,4,-1],
-    [-2,-1,3]
-],dtype=np.float64)
+if __name__ == '__main__':
+    A = np.array([
+        [5, -4, 1],
+        [-4, 6, -4],
+        [1, -4, 7]
+    ], dtype=np.float64)
+    V_a = np.array([1.0, 0.0, 0.0], dtype=np.float64)
+    B = np.array([
+        [25, -41, 10, -6],
+        [-41, 68, -17, 10],
+        [10, -17, 5, -3],
+        [-6, 10, -3, 2]
+    ], dtype=np.float64)
+    V_b = np.array([1.0, 0.0, 0.0, 0.0], dtype=np.float64)
+    er = 0.00001
+    print er
+    u, l, k = get_u(A, V_a, er)
+    print "iteration:%d, l:%0.20f " %(k, l), "x:",u
+    u, l, k = get_u(B, V_b, er)
+    print "iteration:%d, l:%0.20f " % (k, l), "x:", u
 
-B = np.array([
-    [6, 2, 1],
-    [2, 3, 1],
-    [1, 1, 1]
-],dtype=np.float64)
-S = B - 7*np.eye(3)
-v = np.array([1,0.5,0.2],dtype=np.float64)
-t = np.array([ 1,0.5228998,0.24219153],dtype=np.float64)
-print B.dot(t)
-get_u_r(S,v)
+# A = np.array([
+#     [7,3,-2],
+#     [3,4,-1],
+#     [-2,-1,3]
+# ],dtype=np.float64)
+#
+# B = np.array([
+#     [6, 2, 1],
+#     [2, 3, 1],
+#     [1, 1, 1]
+# ],dtype=np.float64)
+# S = B - 7*np.eye(3)
+# v = np.array([1,0.5,0.2],dtype=np.float64)
+# t = np.array([ 1,0.5228998,0.24219153],dtype=np.float64)
+# print B.dot(t)
+# get_u_r(S,v)
